ba38be79285db0bfaf0ff6b5a39513f275d9d436
Lars Nielsen <lnc13.lars@gmail.com>
2015-08-25 15:27:22 +0200

    encode_decode test

--- benchmarks/benchmark_create_coder.js
+++ benchmarks/benchmark_create_coder.js
@@ -3,7 +3,12 @@
 // See accompanying file LICENSE.rst or
 // http://www.steinwurf.com/licensing
 
-// Load benchmark framework
+// Load unit testing framework
+var fs = require('fs')
+var path = require('path')
+filedata = fs.readFileSync(path.join(__dirname, 'shoulda.js'),'utf8')
+eval(filedata)
+
 
 
 kodo = module.require('kodo.js');
@@ -17,6 +22,8 @@ var encoder_test = [];
 var decoder_test = [];
 var decoder_factory_test = [];
 var encoder_factory_test = [];
+
+var encode_decode_test = [];
 
 // Setup and execute test
 setup_test(10, 16, 160);
@@ -36,6 +43,7 @@ function run_test(){
     run_encoder_tests();
     run_decoder_factory_tests();
     run_encoder_factory_tests();
+    run_encode_decode_tests();
 }
 
 function run_decoder_tests(){
@@ -67,6 +75,15 @@ function run_decoder_factory_tests(){
         decoder_factory_creation_test(base_id + i);
     }
 }
+
+function run_encode_decode_tests(){
+    var base_id  = "encode_decode_test_";
+    for(var i = 0; i < test_amount; i++){
+        encode_decoded_test(base_id + i);
+    }
+}
+
+// Benchmark functions
 
 function decoder_creation_test(test_id, decoder_factory){
     var start_time = new Date().getTime();
@@ -100,6 +117,58 @@ function encoder_factory_creation_test(test_id){
     encoder_factory_test.push([test_id, test_time]);
 }
 
+function encode_decoded_test(test_id){
+    var encoder_factory = new kodo.encoder_factory(symbols, symbol_size);
+    var encoder = encoder_factory.build();
+
+    var decoder_factory = new kodo.decoder_factory(symbols, symbol_size);
+    var decoder = decoder_factory.build();
+
+    var data_in = random_string(encoder.block_size());
+
+    encoder.set_symbols(data_in);
+
+    var number_of_packets = 0;
+
+    var start_time = new Date().getTime();
+
+    while(!decoder.is_complete()){
+        var packet = encoder.write_payload();
+        number_of_packets = number_of_packets + 1;
+        decoder.read_payload(packet);
+
+        // Make sure we don't get an infinite loop.
+        assert.isTrue(number_of_packets <= symbols);
+    }
+
+    var end_time = new Date().getTime();
+    var test_time = end_time - start_time;
+
+    encode_decode_test.push([test_id, test_time]);
+
+    data_out = decoder.copy_from_symbols();
+    assert.arrayEqual(data_in, data_out);
+}
+
+
+
+// Helper functions
+function random_string(length) {
+    var s = ""
+
+    var random_char=function() {
+        var n = Math.floor(Math.random() * 62)
+        if(n < 10) return n //1-10
+        if(n < 36) return String.fromCharCode(n + 55) //A-Z
+        return String.fromCharCode(n + 61) //a-z
+    }
+
+    while(s.length < length) s += random_char()
+    return s
+}
+
+
+// Print functions
 
 function print_all_tests(){
     print_decoder_tests();
@@ -109,6 +178,8 @@ function print_all_tests(){
     print_decoder_factory_tests();
     console.log("\n");
     print_encoder_factory_tests();
+    console.log("\n");
+    print_encode_decode_tests();
 }
 
 function print_test_average_time(data){
@@ -152,6 +223,15 @@ function print_encoder_factory_tests(){
         print_test_output(entry);
     });
     print_test_average_time(encoder_factory_test);
+}
+
+function print_encode_decode_tests(){
+    console.log("Create decode encode benchmark");
+    print_symbol_and_symbol_size();
+    encode_decode_test.forEach(function(entry){
+        print_test_output(entry);
+    });
+    print_test_average_time(encode_decode_test);
 }
 
 
